bdd9cd7155a7bb8c88e4548338264e30f0963572
Sangram Raje <sangram@igeek.in>
2014-05-17 01:16:25 -0400

    cleanup and better JSON structure

--- nirmalbang/parseContractNote.py
+++ nirmalbang/parseContractNote.py
@@ -15,6 +15,8 @@ global line_data
 global line_data_map
 global line_type
 global complete_file_data
+global trades_details
+global summary_map
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
@@ -29,6 +31,8 @@ class MyHTMLParser(HTMLParser):
     global line_data_map
     global line_type
     global complete_file_data
+    global trades_details
+    global summary_map
 
     if trade_date_on_next_td:
 #      print "Encountered a start tag:", tag
@@ -41,13 +45,24 @@ class MyHTMLParser(HTMLParser):
         order_info_line_count = order_info_line_count + 1
         field_number = -1
         if order_info_line_count % 3 == 2 or line_type == 'Summary':
-          order_info_start = True
-          line_type = 'SecName'
           if order_info_line_count > 3:
-            print "LINE: ", line_data
-            json_line = json.dumps(line_data_map)
-            print json_line
-            complete_file_data = complete_file_data + [line_data_map]
+            #print "LINE: ", line_data
+            #json_line = json.dumps(line_data_map)
+            #print json_line
+            if line_type == 'Summary':
+              if len(line_data_map) == 2:
+                key = line_data_map['Summary##Security']
+                if line_data_map.has_key('Summary##STT Amount'):
+                  val = line_data_map['Summary##STT Amount']
+                else:
+                  val = line_data_map['Summary##Amount Rs']
+                summary_map[key] = val
+              complete_file_data['Summary'] = summary_map
+            else:
+              trades_details = trades_details + [line_data_map]
+              complete_file_data['Trades'] = trades_details
+          order_info_start = True
+          line_type = 'TradeInfo'
           line_data = ''
           line_data_map = {}
         elif order_info_line_count % 3 == 0 and order_info_line_count > 0:
@@ -56,7 +71,7 @@ class MyHTMLParser(HTMLParser):
           line_type = 'Total_STT'
       elif order_info_start and tag.startswith('td'):
         field_number = field_number + 1
-        if field_number > 0 and line_type == 'SecName' and line_data == '':
+        if field_number > 0 and line_type == 'TradeInfo' and line_data == '':
           line_type = 'Summary'
 
 
@@ -65,14 +80,14 @@ class MyHTMLParser(HTMLParser):
     global order_info_start_row
     global complete_file_data
     if order_info_start and tag.startswith('table'):
-      print "Encountered an end tag :", tag
+      #print "Encountered an end tag :", tag
       order_info_start = False
       order_info_start_row = False
       json_line = json.dumps(complete_file_data, sort_keys = True, indent = 4, separators = (',', ': '))
-      print "Complete Data"
+      #print "Complete Data"
       print json_line
-    if 0:
-      print "Encountered an end tag :", tag
+    #if 0:
+    #  print "Encountered an end tag :", tag
 
   def handle_data(self, data):
     global trade_date_next
@@ -94,14 +109,15 @@ class MyHTMLParser(HTMLParser):
 #      print date_str_arr
       trade_date = int(date_str_arr[2]) * 10000 + int(date_str_arr[1]) * 100 + int(date_str_arr[0])
       trade_date_next = False
-      print "setting trade_date to " + str(trade_date)
+      #print "setting trade_date to " + str(trade_date)
+      complete_file_data['trade_date'] = trade_date
     if data.startswith('TRADE DATE :'):
 #      print "Encountered some data  :", data
       trade_date_on_next_td = True
 #      print "setting trade_date_on_next_td"
 
     if order_info_start and len(data.strip()) > 0:
-      if line_type == 'SecName':
+      if line_type == 'TradeInfo':
         line_data_map[FIELDS_ARR[field_number]] = data.strip()
       elif line_type == 'STT_Info' or line_type == 'Total_STT':
         line_data_map[line_type + '##' + FIELDS_ARR[field_number]] = data.strip()
@@ -111,13 +127,12 @@ class MyHTMLParser(HTMLParser):
 
 
     if data.startswith('Order No'):
-      print "Encountered some data  :", data
+      #print "Encountered some data  :", data
       order_info_start_row = True
       order_info_start = False
-      line_type = 'SecName'
+      line_type = 'TradeInfo'
       line_data = ''
       line_data_map = {}
-      complete_file_data = []
 
 
 
@@ -134,6 +149,10 @@ def callParser(filename):
   global order_info_start
   global order_info_line_count
   global FIELDS_ARR
+  global complete_file_data
+  global trades_details
+  global summary_map
+
   file = open(filename, 'r')
   entire_file = file.read()
   trade_date = 19700101
@@ -144,6 +163,9 @@ def callParser(filename):
   order_info_start = False
   FIELDS_ARR = ['Order No', 'Order Time', 'Trade No.', 'Trade Time', 'Security', 'Bought Qty', 'Sold Qty', 'Gross Rate Per Security',
       'Gross Total', 'Brokerage', 'Net Rate Per Security', 'Service Tax', 'STT Amount', 'Amount Rs']
+  complete_file_data = {}
+  trades_details = []
+  summary_map = {}
   parser.feed(entire_file)
 
 def print_data() :
